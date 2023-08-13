6e06c47673b95e7a8f705e1f2570ca41b9e97518
nemi <nemimj123@gmail.com>
2023-08-13 12:41:41 +0530

    added footer and reponsive to the website

--- EdgeLedger Website/js/main.js
+++ EdgeLedger Website/js/main.js
@@ -9,3 +9,25 @@ function initMap() {
   // The marker, positioned at location
   const marker = new google.maps.Marker({ position: loc, map: map });
 }
+
+// Smooth Scrolling
+$("#navbar a, .btn").on("click", function (ev) {
+  if (this.hash !== "") {
+    ev.preventDefault();
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
+
+// Sticky menu background
+window.addEventListener("scroll", function () {
+  if (this.window.scrollY > 150)
+    this.document.querySelector("#navbar").style.opacity = 0.8;
+  else this.document.querySelector("#navbar").style.opacity = 1;
+});
